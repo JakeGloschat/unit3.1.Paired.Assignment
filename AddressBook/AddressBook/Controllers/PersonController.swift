04b4f3663f7bd6ff227171e050d61cf37b70d37c
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-13 15:48:27 -0700

    Added CRUD functions to PersonController

--- AddressBook/AddressBook/Controllers/PersonController.swift
+++ AddressBook/AddressBook/Controllers/PersonController.swift
@@ -17,17 +17,20 @@ class PersonController {
     
     // MARK: - CRUD
     
-    static func create(name: String = "New Contact", address: String = "", group: Group) {
+    static func createPerson(name: String = "New Contact", address: String = "", group: Group) {
         let person = Person(name: name, address: address)
         group.people.append(person)
+        // Don't forget to call the save function once made
     }
-    
-    static func update() {
-        
+    static func updatePerson(newName: String, newAddress: String, newPerson: Person) {
+        newPerson.name = newName
+        newPerson.address = newAddress
+        // Don't forget to call the save function once made
     }
-    
-    static func delete() {
-        
+    static func delete(personToDelete: Person, from collection: Group) {
+        guard let index = collection.people.firstIndex(of: personToDelete) else {return}
+        collection.people.remove(at: index)
+        // Don't forget to call the save function once made
     }
     
     
