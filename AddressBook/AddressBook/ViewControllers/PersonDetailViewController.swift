dea39cc195cf3df88f324d4029ac600ea86ff21c
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-13 22:52:16 -0700

    Created the detail view controller, added lifecycle and helper functions.

--- AddressBook/AddressBook/ViewControllers/PersonDetailViewController.swift
+++ AddressBook/AddressBook/ViewControllers/PersonDetailViewController.swift
@@ -9,14 +9,37 @@ import UIKit
 
 class PersonDetailViewController: UIViewController {
     
+    // MARK: - Outlets
+    
+    @IBOutlet weak var personNameTextField: UITextField!
+    @IBOutlet weak var personAddressTextField: UITextField!
+    
+    
     // MARK: - Properties
     var personObjectReceiver: Person?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+    // MARK: - helper function
+    
+    func updateViews() {
+        guard let personObjectReceiver = personObjectReceiver else { return }
+        personNameTextField.text = personObjectReceiver.name
+        personAddressTextField.text = personObjectReceiver.address
+    }
+    // MARK: - Actions
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let person = personObjectReceiver,
+              let name = personNameTextField.text,
+              let address = personAddressTextField.text else { return }
+        PersonController.updatePerson(newName: name, newAddress: address, newPerson: person)
+        navigationController?.popViewController(animated: true)
         
     }
-        
-        
+    
+    
     
 } // End of Class
